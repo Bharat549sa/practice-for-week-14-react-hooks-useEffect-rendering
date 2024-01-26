8b55b667f94ee19d3efed8b42b40a82187143c67
brian549sa <81371017+Bharat549sa@users.noreply.github.com>
2024-01-26 20:47:14 +0000

    hi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
+  const [toggleOne, setToggleOne] = useState(false);
+ 
   return (
+    <button onClick={() => setToggleOne(!toggleOne)}>ToggleOne</button>;
+    useEffect(() => {
+      console.log('UseEffect1 Ran');
+    }, []);
     <>
       <h1>Hello App</h1>
     </>
